ce104934cdb7a9e65588f77fd1f18a59d5c1ca8d
Ivan Munteanu <4423975+zverbatim@users.noreply.github.com>
2018-10-07 16:27:45 -0500

    Update pass reset route

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,4 +3,4 @@ export const SIGN_IN = "/signin";
 export const LANDING = "/";
 export const HOME = "/home";
 export const ACCOUNT = "/account";
-export const PASSWORD_FORGET = "/pw-forget";
+export const PASSWORD_FORGET = "/password_forget";
